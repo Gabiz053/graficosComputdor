29790b0b9c4ac728e17449bfaf1d859d83c3a27a
Gabriel Gómez García <ggg239@alumnos.unican.es>
2024-11-25 13:52:54 +0100

    Prueba de test y comprobado que funciona todo

---
+++ practica02/src/__init__.py
@@ -0,0 +1,9 @@
+from .constantes import Default
+from .algoritmos_dibujo import AlgoritmoDibujo
+from .ventana_menu_canvas_animacion import VentanaMenuCanvasAnimacion
+
+__all__ = [
+    "Default",
+    "AlgoritmoDibujo",
+    "VentanaMenuCanvasAnimacion",
+]
